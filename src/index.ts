9e20c0b77a295a9918380724577667cdc045b8a2
Joe Hildebrand <joe-github@cursive.net>
2025-05-19 12:31:31 -0600

    Add isCI.  Update dependencies.

--- src/index.ts
+++ src/index.ts
@@ -29,3 +29,31 @@ export function errCode(e: unknown, code: string | number): boolean {
   }
   throw new TypeError(`Invalid code: ${JSON.stringify(code)}`);
 }
+
+export interface CiOptions {
+  CI?: boolean;
+}
+
+/**
+ * Is the current environment CI-like.
+ *
+ * @param opts Override the environment determination, for testing.
+ * @returns True if in CI.
+ */
+export function isCI(opts?: CiOptions): boolean {
+  // Override for testing, so we don't have to much with process.env
+  if (opts && ('CI' in opts)) {
+    return Boolean(opts.CI);
+  }
+
+  const {env} = process;
+  return Boolean(
+    // Travis CI, CircleCI, Cirrus CI, Gitlab CI, Appveyor, CodeShip, dsari,
+    // GitHub Actions
+    env.CI ||
+    env.CONTINUOUS_INTEGRATION || // Travis CI, Cirrus CI
+    env.BUILD_NUMBER || // Jenkins, TeamCity
+    env.RUN_ID || // TaskCluster, dsari
+    false
+  );
+}
